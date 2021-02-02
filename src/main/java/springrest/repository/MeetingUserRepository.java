4b7c6683a3b6e90633c123fa3296573f75255050
Peters <vpe@iks-gmbh.com>
2021-02-02 13:59:53 +0100

    deleted unused methods from MeetingUserRepository

--- src/main/java/springrest/repository/MeetingUserRepository.java
+++ src/main/java/springrest/repository/MeetingUserRepository.java
@@ -4,16 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import springrest.entity.MeetingUser;
 
 import javax.transaction.Transactional;
-import java.util.List;
 import java.util.Optional;
 
 public interface MeetingUserRepository extends JpaRepository<MeetingUser, Integer> {
 
-  List<MeetingUser> findByIdUser(int id);
-
   Optional<MeetingUser> findByIdUserAndIdMeeting(int userId, int meetingId);
-
-  List<MeetingUser> findByIdMeeting(int id);
 
   @Transactional
   void deleteByIdUserAndIdMeeting(int userId, int meetingId);
