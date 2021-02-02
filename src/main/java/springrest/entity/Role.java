06bc689b1d3ba9247f831b07539faa6ae414ff16
Peters <vpe@iks-gmbh.com>
2021-02-02 14:01:58 +0100

    deleted unused constructors and methods from Role

--- src/main/java/springrest/entity/Role.java
+++ src/main/java/springrest/entity/Role.java
@@ -6,10 +6,6 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "role")
@@ -26,22 +22,5 @@ public class Role {
   @Enumerated(EnumType.STRING)
   @Column(length = 20)
   private ERole name;
-
-  public Role(ERole name) {
-    this.name = name;
-  }
-
-  public Role(List<String> roles) {
-    System.out.println("ROLES: " + roles);
-  }
-  public static Set<Role> convertStringSetToRoleSetWithStreams(List<String> roles) {
-    Set<String> rolesSet = new HashSet<>(roles);
-
-    return rolesSet.stream().map(roleInString -> {
-      final Role role = new Role();
-      role.setName(ERole.valueOf(roleInString));
-      return role;
-    }).collect(Collectors.toSet());
-  }
 
 }
